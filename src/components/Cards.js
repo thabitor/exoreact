c253475cd4d626eba2b292881036e64f7711fc16
thabitor <thabithamror@gmail.com>
2023-11-01 22:09:43 +0100

    first publish

---
+++ src/components/Cards.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cards() {
+  return (
+    <div className='cards'>
+      <h1>Check out these EPIC Destinations!</h1>
+    </div>
+  )
+}
+
+export default Cards
