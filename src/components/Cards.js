e39fc5406a259e8e1281852106aa4bd87b56d15e
thabitor <thabithamror@gmail.com>
2023-11-02 00:32:37 +0100

    Added card items

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import CardItem from './CardItem'
+import './Cards.css'
 
 function Cards() {
-  return (
-    <div className='cards'>
-      <h1>Check out these EPIC Destinations!</h1>
-    </div>
-  )
+    return (
+        <div className='cards'>
+            <h1>Check out these EPIC Destinations!</h1>
+            <div className='.cards__container'>
+                <div className='cards__wrapper'>
+                    <ul className='cards__items'>
+                        <CardItem 
+                        src='images/img-9.jpg' 
+                        text='Explore the hidden waterfall deep inside the Amazon Jungle!'
+                        label='Adventure'
+                        path='/services'
+                        />
+                        <CardItem 
+                        src='images/img-2.jpg' 
+                        text='Travel through the Islands of Bali in a Private Cruise'
+                        label='Luxury'
+                        path='/services'
+                        />
+                        <CardItem 
+                        src='images/img-3.jpg' 
+                        text='Experience Football on Top of the Himalayan Mountains'
+                        label='Adrelanine'
+                        path='/services'
+                        />
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Cards
